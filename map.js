77b4c9e9b67cdb9c253fcd6f57d50105c3667878
Naman Krishnatre <naman2005krs@gmail.com>
2025-04-23 12:13:05 +0530

    First Commit

---
+++ map.js
@@ -0,0 +1,26 @@
+const Fruits = new Map([
+    ["Apples",500],
+    ["Bananas",300],
+    ["Oranges",200]
+]);
+console.log(fruits);
+
+//Create a Map
+const fruits = new Map();
+
+    //Set Map values
+    fruits.set("Apples",500);
+    fruits.set("Bananas",300);
+    fruits.set("Oranges",200);
+    //the set() methi can be used to change existing map values
+    
+    fruits.get("Apples");
+
+//toosley coupled language
+
+//Return Object
+//type of fruits;
+//instanceof Map returns true;
+//Return true
+//fruits instanceof Map
+
