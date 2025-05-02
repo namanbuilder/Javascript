8c36423ee64f00c7eae9ff375033e83be1972f85
Naman Krishnatre <naman2005krs@gmail.com>
2025-05-02 12:26:22 +0530

    async and await

--- concurrency.js
+++ concurrency.js
@@ -1,4 +1,5 @@
-//callbacks-function pass as an argument 
+//callbacks-its just a fucntion which takes another function as an argument
+// which is then called when the rest of the initial function as an argument.function pass as an argument 
 // function doSomething(callback) {
 //     callback();
 // }
@@ -67,28 +68,74 @@
 //         console.log("Error:", error);
 //     }
 //   );
- const promise = new Promise((resolve, reject) =>{
-    reject("Success!");
-})
-.then(value => {
-    console.log(value);
-    return "we";
-})
-.then(value => {
-    console.log(value);
-    return"can";
-})
-.then(value => {
-    console.log(value);
-    return "chain";
-})
-.then(value => {
-    console.log(value);
-    return "promise";
-})
-.then(value => {
-    console.log(value);
-})   
-    .catch(value => {
-        console.log(value);
-})
+//  const promise = new Promise((resolve, reject) =>{
+//     reject("Success!");
+// })
+// .then(value => {
+//     console.log(value);
+//     return "we";
+// })
+// .then(value => {
+//     console.log(value);
+//     return"can";
+// })
+// .then(value => {
+//     console.log(value);
+//     return "chain";
+// })
+// .then(value => {
+//     console.log(value);
+//     return "promise";
+// })
+// .then(value => {
+//     console.log(value);
+// })   
+//     .catch(value => {
+//         console.log(value);
+// })
+
+// function doSomething(callback) {
+//     callback();
+// }
+// function sayHi(){
+//     console.log("Hi!");
+// }
+// doSomething(sayHi);
+ 
+//async and await
+
+// with the async keyword we can make a function return a promise, this makes the promises nicer to read and look a lot like synchronous (non-concurrent code)
+
+// function saySomething(x) {
+//     return new Promise(() =>{
+//         setTimeout(() => {
+//             resolve("something"+x);
+//         },2000);
+//     });
+// }
+// async function talk(x) {
+//     const words = await saySomething(x);
+//     console.log(words);
+
+//     }
+//      talk (2);
+//      talk (4);
+//      talk (8);
+
+//event loop - it is a single-threaded language. a thread in this context means a  path of execution. 
+// if there is only a single path, this means that tasks will hve to wait for another and only one thing canhappen at a time.
+ //call stack and callback queue- the event loop is a process that is constantly monitoring this call stack, and whenever
+// there are to do, the event loop does them one by one and the task from the top gets executed first.
+ 
+// console.log("Hi there")
+// add(4,5);
+// console.log(add)
+// function add(x,y){
+//     return x+y
+// }
+console.log("Hi there")
+setTimeout(( => console.log("sorry I'm late" 100)));
+console.log(add(4,5));
+function add(x,y){1
+    return x+y
+}
